d90ddbde96056c1b834c1b9a46867a31861f9027
Jason Ardell <ardell@gmail.com>
2015-12-07 08:14:57 -0700

    Set up cocoapod.

---
+++ Example/Pods/Target Support Files/BackgroundTask/BackgroundTask-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BackgroundTask : NSObject
+@end
+@implementation PodsDummy_BackgroundTask
+@end
---
+++ Example/Pods/Target Support Files/BackgroundTask/BackgroundTask-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double BackgroundTaskVersionNumber;
+FOUNDATION_EXPORT const unsigned char BackgroundTaskVersionString[];
+
